6461f131cb1368f348171c198678c6cc5e5f8030
Unlimited-hy <44118543+Unlimited-hy@users.noreply.github.com>
2023-03-09 15:19:57 +0800

    Update SKS.hpp

--- C++ Codes/modules/CPU_modules/SKS.hpp
+++ C++ Codes/modules/CPU_modules/SKS.hpp
@@ -15,9 +15,9 @@ namespace sks {
 	*/
 
     int runKernel_ACA(float* src, float* tar, float* result);
-    int runKernel_ACA_double(float* src, float* tar, double* result);
+    int runKernel_ACA_double(double* src, double* tar, double* result);
     int runKernel_SKS(float* src, float* tar, float* result);
-    int runKernel_SKS_double(float* src, float* tar, double* result);
+    int runKernel_SKS_double(double* src, double* tar, double* result);
 
 }
 
