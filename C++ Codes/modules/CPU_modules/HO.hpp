1e972dc628a5e8c45f5805cfd691b89baa86fef6
Unlimited-hy <44118543+Unlimited-hy@users.noreply.github.com>
2023-03-12 15:19:02 +0800

    Update HO.hpp

--- C++ Codes/modules/CPU_modules/HO.hpp
+++ C++ Codes/modules/CPU_modules/HO.hpp
@@ -3,7 +3,7 @@
 #define __OPENCV_HO_H__
 #include <opencv2/core.hpp>
 
-namespace my_ho
+namespace cv
 {
 	void runKernel_HO(cv::InputArray _srcPoints, cv::InputArray _targPoints, cv::Matx33d& H);
 }
